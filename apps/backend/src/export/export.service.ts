49c52aa4467340e1c18e223161722112f2482fb5
Stefano Solidoro <kishosa@me.com>
2026-02-11 09:42:53 +0100

    Frontend UI EXPORT

--- apps/backend/src/export/export.service.ts
+++ apps/backend/src/export/export.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { VisitStatus } from '@prisma/client';
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const PDFDocument = require('pdfkit');
+import PDFDocumentModule from 'pdfkit';
+const PDFDocument = (PDFDocumentModule as any).default || PDFDocumentModule;
 import { format } from 'date-fns';
 import { it } from 'date-fns/locale';
 
